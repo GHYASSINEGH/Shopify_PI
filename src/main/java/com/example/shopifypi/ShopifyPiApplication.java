c754f9e5c3b865309e1baf94485a4ba6d2e0f7a8
GHODHBANI Yassine <yassine.ghodhbani1@esprit.tn>
2023-02-21 11:45:38 +0100

    First Push

---
+++ src/main/java/com/example/shopifypi/ShopifyPiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopifypi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopifyPiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopifypi/ShopifyPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopifypi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
